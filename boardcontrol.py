98d89483309f868ac2cc259647604211766f596a
Sand Paper <chankinwang8@gmail.com>
2023-11-25 16:50:13 +0800

    test commiting from terminal nothing is changed

--- boardcontrol.py
+++ boardcontrol.py
@@ -3,10 +3,16 @@ import os, math
 boardState = []
 
 def initialBoard():
+    # generate the empty 2d grid
     for i in range(4):
         boardState.append([])
         for j in range(4):
             boardState[i].append(" ")
+    
+    # create 2 tiles to begin
+    for i in range(2):
+        create_tile()
+
     print_board(boardState)
 
 def print_board(a):
@@ -32,3 +38,17 @@ def print_board(a):
             print("|",end="")
         print()
         print("--------------------")
+
+# create a tile of either value 2 or 4 on an empty space
+def create_tile():
+    pass
+
+# create an obstacle
+def create_obstacle():
+    pass
+
+# each tile, from left to right, from top to bottom, assigned id of 0 to 15
+# id_to_coord() converts id to the coord of the grid
+# eg id 5 -> grid[1][1]
+def id_to_coord():
+    pass
