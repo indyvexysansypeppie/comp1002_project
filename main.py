8f8438174f10f50f639b68234ececcb742819eaf
steph-hhc <144767793+steph-hhc@users.noreply.github.com>
2023-11-26 21:30:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import boardcontrol as bcon
 
 bcon.initialBoard()
+
+score = 0
+highest_tile = 0
+
+while True:
+    print_board(board)
+    print("Score:", score)
+    print("Highest Tile:", highest_tile)
+    direction = get_arrow_key()
+    move(board, direction)
+    add_new_tile(board)
