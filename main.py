327ea4eb6e24ea5d5063e4713909750a17679372
Perry <38996133+0oPerryo0@users.noreply.github.com>
2023-11-27 19:20:33 +0800

    added winning condition and game over

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ score = 0
 highest_tile = 0
 
 while True:
-    bcon.breakObstacle()
-    bcon.clear_screen()
-    bcon.print_board()
-    print("Score:", score)
-    print("Highest Tile:", highest_tile)
-    direction = bcon.get_arrow_key()
-    bcon.move(direction)
-    bcon.create_tile()
-    score = bcon.pass_score()
-    highest_tile = bcon.pass_highest_tile()
+    try:
+        if highest_tile == 2048:
+            print("You won")
+            break
+
+        bcon.breakObstacle()
+        bcon.clear_screen()
+        bcon.print_board()
+        print("Score:", score)
+        print("Highest Tile:", highest_tile)
+        direction = bcon.get_arrow_key()
+        bcon.move(direction)
+        bcon.create_tile()
+        score = bcon.pass_score()
+        highest_tile = bcon.pass_highest_tile()
+        
+    except TypeError:
+        print("Out of Blocks!")
+        break
