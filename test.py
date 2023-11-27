3580102866257386aa8a5ab083c858422356fbf5
indyvexysansypeppie <115988159+indyvexysansypeppie@users.noreply.github.com>
2023-11-27 12:10:22 +0800

    fixed the whole thing

--- test.py
+++ test.py
@@ -2,33 +2,40 @@ import os, math, random, msvcrt
 
 # row = [0, 0, 127, 0]
 # row = [2, 0, 127, 0]
-row = [0, 2, 127, 0]
-# row = [0, 127, 4, 16]
+# row = [0, 2, 127, 0]
+# row = [0, 127, 4, 4]
 # row = [127, 0, 4, 4]
-# row = [0, 0, 0, 127]
-# row = [127, 127, 0, 4]
+# row = [0, 2, 2, 127]
+row = [127, 4, 127, 4]
 
 splitRow = [[]]
 obstacles = {}
 n=0
-for i in range(1,4): # check each value in row
+for i in range(4): # check each value in row
     if row[i] == 0: # skip over 0s
         continue
     elif math.ceil(math.log2(row[i])) != math.floor(math.log2(row[i])): # check if value != 2^x aka obstacle
         n+=1
         obstacles[i]=row[i]
+        # print(obstacles)
         splitRow.append([])
         continue
     splitRow[n].append(row[i])
 
+# print(splitRow)
+
 output = []
-tileCount = 0
 i=0
 while True:
-    if splitRow[i] != []:
+    tileCount = 0 # count no of tiles in that split list
+    if splitRow[i] != []: # empty lists are discarded
         for j in splitRow[i]:
             output.append(j)
             tileCount += 1
+            if (tileCount >= 2) and (output[-1] == output[-2]): # if there are >2 tiles and both tiles are same value
+                output[-2] *= 2
+                output[-1] = 0
+                tileCount-1
     i+=1
     if i == len(splitRow):
         break
